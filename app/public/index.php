97cd9860e85686b80f2d56d6a83d0f69779dc4a6
oleh_ok <oleh.k@hotmail.com>
2023-11-06 20:56:46 +0200

    build table of users

--- app/public/index.php
+++ app/public/index.php
@@ -8,3 +8,15 @@ $database = new DatabaseConnection();
 $database->connect();
 $users = $database->getUsers();
 
+echo '<table>';
+echo '<tr><th>name</th><th>desc</th></tr>';
+
+foreach ($users as $key => $user) {
+
+    $name = $user['name'];
+    $desc = $user['desc'];
+
+    echo "<tr><td>$name</td><td>$desc</td></tr>";
+
+}
+echo '</tr>';
